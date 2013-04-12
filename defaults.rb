78552a747122eb5f911ae2b5cadb3a38fec6501a
Marcella <marcellahastings@gmail.com>
2013-04-12 18:18:53 -0400

    started collecting phrases

---
+++ defaults.rb
@@ -0,0 +1,12 @@
+require 'set'
+
+ming = Set.new [ "you're gonna wanna git commit",
+                 "yeahhh..."]
+
+norman = Set.new [ "pointy-headed type theorists",
+                   "this is all just syntactic sugar for lambda",
+                   "this is unutterably depressing"]
+
+ben = Set.new [ "dead squirrels!",
+                "I'm aggressively nice. "]
+                  
