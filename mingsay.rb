e3f74b37d7d79ec25a890119c878ec7722907d2f
Marcella <marcellahastings@gmail.com>
2013-04-13 10:29:10 -0400

    art + better error

--- mingsay.rb
+++ mingsay.rb
@@ -1,24 +1,13 @@
 #!/usr/bin/env ruby
 
-# to do
-# -nr flag
-
 def putline(line,width,left,right)
     print left + " "
     print line + " "*(width-line.size)
     print " " + right + "\n"
 end
 
-def errormessage()
-  puts "Usage: echo 'text' | ./mingsay.rb -flag\n"
-  puts "Flags: -nr \n"
-  puts "       -cow \n"
-  puts "       -ming \n"
-  puts "       -ben\n"
-  puts "       -mark\n"
-end
 
-people = {"-ming" =>
+$people = {"-ming" =>
           [
             "\\     ___",
             " \\   /    \\",
@@ -43,21 +32,29 @@ people = {"-ming" =>
             "        ||     ||"
           ],
           "-ben"=>[
-            "     _____",
-            "\\   /  __ \\",
-            " \\ |__/)~ |",
-            "  \\| n  n |",
-            "   |  __  |",
-            "    \\____/",
+            "\\     ____",
+            " \\   /  __\\",
+            "  \\ |__/)~ |",
+            "   \\| n  n |",
+            "    |  __  |",
+            "     \\____/",
           ],
           "-mark"=>[
-            "     ____",
-            "\\   /    \\",
-            " \\ | _  _ |",
-            "  \\| .  . |",
-            "   |  ~~  |",
-            "    \\____/",
+            "\\     ____",
+            " \\   /    \\",
+            "  \\ | _  _ |",
+            "   \\| .  . |",
+            "    |  ~~  |",
+            "     \\____/",
           ]}
+
+def errormessage()
+  puts "Usage: echo 'text' | ./mingsay.rb [-flag]\n"
+  print "Flags:"
+  $people.each do |p, v|
+    puts "\t" + p
+    end
+end
 
 def putming(offset, person)
     person.each do |line|
@@ -88,12 +85,12 @@ nargs = ARGV.size
 
 if (nargs == 0)
   text = ARGF.read.split("\n")
-  draw(text, people["-ming"])
+  draw(text, $people["-ming"])
 elsif (nargs == 1)
   option = ARGV.shift
-  if people.has_key?(option)
+  if $people.has_key?(option)
     text = ARGF.read.split("\n")
-    draw(text, people[option])
+    draw(text, $people[option])
   else
     errormessage()
   end
