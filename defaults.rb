2d339796787a2ce8b7bff89cde83ef24ed394202
cblanck <connorblanck@gmail.com>
2013-04-13 23:27:17 -0300

    Look I'm helping
    So much

--- defaults.rb
+++ defaults.rb
@@ -5,7 +5,8 @@ ming = Set.new [ "you're gonna wanna git commit",
 
 norman = Set.new [ "pointy-headed type theorists",
                    "this is all just syntactic sugar for lambda",
-                   "this is unutterably depressing"]
+                   "this is unutterably depressing",
+                   "I can't hear you over the sound of your leaky abstraction!"]
 
 ben = Set.new [ "dead squirrels!",
                 "I'm aggressively nice. "]
