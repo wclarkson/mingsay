773f614a309f495ae53d1261a44ace5c97de5543
Will Clarkson <william.clarkson@tufts.edu>
2013-05-14 23:51:59 -0400

    moved ascii art and quotations to external files. todo: command line argument handling with some modicum of grace

--- mingsay.rb
+++ mingsay.rb
@@ -1,100 +1,61 @@
 #!/usr/bin/env ruby
 
-def putline(line,width,left,right)
-    print left + " "
-    print line + " "*(width-line.size)
-    print " " + right + "\n"
-end
+class Mingsay
+    attr_accessor :name, :image, :quote, :quotations
 
+    def initialize(name)
+        @name = name
+        path = File.expand_path(ENV['MINGSAY_PATH'])
+        if !File.directory?(path) then
+            puts "No mingsay directory found."
+            exit
+        end
+        begin
+            @image = File.open(path+'/people/'+@name, 'r').read
+            img_shift = 8
+            @image = @image.split("\n").map {|l| " "*img_shift+l}
+            @quotations = File.open(path+'/quotations/'+@name, 'r')
+                              .read.split("\n\n").map(&:chomp)
+        rescue
+            puts "No data for specified person found in mingsay directory."
+            exit
+        end
+    end
 
-$people = {"-ming" =>
-          [
-            "\\     ___",
-            " \\   /    \\",
-            "  \\ |`[][]-|",
-            "    |  __  |",
-            "     \\    /",
-            "     /----\\"   ],
-          "-nr" =>[
-            "\\    nnnnnn",
-            " \\  (|~  _|)",
-            "  \\ (-[][]-)",
-            "    (| __ |)",
-            "    ((uuuu))",
-            "    ( )  ( )",
-            "    ( )  ( )",
-          ],
-          "-cow"=>[
-            "\\   ^__^",
-            " \\  (oo)\\_______",
-            "    (__)\\       )\\/\\",
-            "        ||----w |",
-            "        ||     ||"
-          ],
-          "-ben"=>[
-            "\\     ____",
-            " \\   /  __\\",
-            "  \\ |__/)~ |",
-            "   \\| n  n |",
-            "    |  __  |",
-            "     \\____/",
-          ],
-          "-mark"=>[
-            "\\     ____",
-            " \\   /    \\",
-            "  \\ | _  _ |",
-            "   \\| .  . |",
-            "    |  ~~  |",
-            "     \\____/",
-          ]}
+    def draw_box(text)
+        text = text.split("\n").map(&:chomp)
+        if (text.size==1) then
+            width = text[0].size
+            puts " " + "_"*(width+2)
+            puts "< " + text[0] + " >"
+            puts " " + "-"*(width+2)
+        else
+            width = text.map {|x| x.size}.max
+            puts " " + "_"*(width+2)
+            putline(text[0],width,"/","\\")
+            for i in 1..(text.size-2)
+                putline(text[i],width,"|","|")
+            end
+            putline(text[-1],width,"\\","/")
+            puts " " + "-"*(width+2)
+        end
+    end
 
-def errormessage()
-  puts "Usage: echo 'text' | ./mingsay.rb [-flag]\n"
-  print "Flags:"
-  $people.each do |p, v|
-    puts "\t" + p
+    def set_quote(quote)
+        @quote = quote
+    end
+
+    def random_quote()
+        @quote = @quotations.sample
+    end
+
+    def print()
+        draw_box(@quote)
+        puts @image
     end
 end
 
-def putming(offset, person)
-    person.each do |line|
-      puts " "*offset + line
-      end
-end
-
-def draw(text, option)
-  if (text.size==1)
-    width = text[0].size
-    puts " " + "_"*(width+2)
-    puts "< " + text[0] + " >"
-    puts " " + "-"*(width+2)
-  else
-    width = text.map {|x| x.size}.max
-    puts " " + "_"*(width+2)
-    putline(text[0],width,"/","\\")
-    for i in 1..(text.size-2)
-        putline(text[i],width,"|","|")
-    end
-    putline(text[-1],width,"\\","/")
-    puts " " + "-"*(width+2)
-  end
-  putming(10, option)
-end
-
-nargs = ARGV.size
-
-if (nargs == 0)
-  text = ARGF.read.split("\n")
-  draw(text, $people["-ming"])
-elsif (nargs == 1)
-  option = ARGV.shift
-  if $people.has_key?(option)
-    text = ARGF.read.split("\n")
-    draw(text, $people[option])
-  else
-    errormessage()
-  end
-else
-  errormessage()
-end
+m = Mingsay.new('nr')
+m.random_quote()
+m.print()
 
